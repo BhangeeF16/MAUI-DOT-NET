268b20e3660f94b8beba8992496892eaf0b77998
arsalan139 <muhammad.arsalan139@gmail.com>
2022-08-11 13:40:47 +0500

    MAUI App

---
+++ SampleApp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	    
+		MainPage = new AppShell();
+
+
+
+	}
+
+}
---
+++ SampleApp/MauiProgram.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Maui.LifecycleEvents;
+using CommunityToolkit.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Xaml;
+#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+#endif
+namespace SampleApp;
+public static class MauiProgram
+{
+
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+        {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+        }).UseMauiCommunityToolkit();
+
+
+
+
+#if WINDOWS
+        builder.ConfigureLifecycleEvents(events =>
+        {
+            events.AddWindows(wndLifeCycleBuilder =>
+            {
+                wndLifeCycleBuilder.OnWindowCreated(window =>
+                {
+                    IntPtr nativeWindowHandle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                   
+                    WindowId win32WindowsId = Win32Interop.GetWindowIdFromWindow(nativeWindowHandle);
+                    
+                    AppWindow winuiAppWindow = AppWindow.GetFromWindowId(win32WindowsId);    
+                   
+                    if(winuiAppWindow.Presenter is OverlappedPresenter p)
+                       { p.Maximize();
+                       p.IsAlwaysOnTop=true;
+                       p.IsResizable=false;
+                       p.IsMaximizable = false;
+                       p.IsMinimizable=false;
+
+ 
+
+                       }
+                        
+                    else
+                    {
+                        const int width = 1200;
+                        const int height = 800;
+                        winuiAppWindow.MoveAndResize(new RectInt32(1920 / 2 - width / 2, 1080 / 2 - height / 2, width, height));
+                       
+                        }
+                        
+                }).OnClosed((window,args)=>HandleClose);
+            });
+        });
+#endif
+        
+        return builder.Build();
+    }
+
+    private static void HandleClose()
+    {
+
+    }
+
+
+
+
+}
